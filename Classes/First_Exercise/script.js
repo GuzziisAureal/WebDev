33bf04d0e79d231b963da637419ea182d1cd3747
monguzzi.matteo <monguzzi.mateo@studenti.salesianisesto.it>
2025-01-28 09:52:47 +0100

    not finished needs revision

---
+++ Classes/First_Exercise/script.js
@@ -0,0 +1,24 @@
+class Distributore{
+
+    constructor () {
+        this._n = 0;
+    }
+
+    aggiungiCapsule(n) {
+        this._n += n;
+    }
+
+    aggiungiCodice(c) {
+
+    }
+
+    eroga(n, c) {
+        if(this._n >= n){
+
+        }
+    }
+
+    report(c){
+        
+    }
+}
