ff5ef78af7ef9427c62412ad2b2ef18e768d3738
monguzzi.matteo <monguzzi.mateo@studenti.salesianisesto.it>
2025-03-25 10:02:40 +0100

    KFC

---
+++ WebDevving/FirstEx/script.js
@@ -0,0 +1,13 @@
+let nElem = document.getElementById('n')
+let random = (Math.random() * 100)+1
+let time = nElem.value 
+
+nElem.addEventListener('change', ){
+
+    setInterval(() => {
+
+    }, 1000
+    );
+    
+}
+
